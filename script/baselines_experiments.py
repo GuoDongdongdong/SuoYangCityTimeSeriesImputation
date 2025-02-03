054c5a690cce7468c472f08beddc53256a46a59d
徐国栋 <a645219093@qq.com>
2025-02-03 22:38:23 +0800

    add all dataset in baseline_exp.

--- script/baselines_experiments.py
+++ script/baselines_experiments.py
@@ -24,7 +24,13 @@ INTERPOLATE = [
     'previous', # LOCF
     'cubic',
 ]
-
+DATASETS = [
+    ('humidity_20per_block_missing.csv', 'depth05_missing'),
+    ('temperature_20per_block_missing.csv', 'temperature_missing'),
+    ('wind_20per_block_missing.csv', 'windSpeed3s_missing'),
+    ('water_20per_block_missing.csv', 'water1_missing'),
+    ('salt_20per_block_missing.csv', 'salt1_missing'),
+]
 def run():
     args = ['-config_file_dir',
             TEMP_FILE_DIR, 
@@ -41,17 +47,20 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(config_file_path)
     os.makedirs(TEMP_FILE_DIR, exist_ok=True)
-    for model_name in MODEL_LIST:
-        config['CommonArgs']['model'] = f'str:{model_name}'
-        if model_name == 'Interpolate':
-            for kind_name in INTERPOLATE:
-                config['Interpolate']['kind'] = f'str:{kind_name}'
+    for dataset_name, targets_name in DATASETS:
+        config['CommonArgs']['dataset_file_name'] = f'str:{dataset_name}'
+        config['CommonArgs']['targets'] = f'str:{targets_name}'
+        for model_name in MODEL_LIST:
+            config['CommonArgs']['model'] = f'str:{model_name}'
+            if model_name == 'Interpolate':
+                for kind_name in INTERPOLATE:
+                    config['Interpolate']['kind'] = f'str:{kind_name}'
+                    with open(os.path.join(TEMP_FILE_DIR, TEMP_CONFIG_FILE_NAME), 'w') as f:
+                        config.write(f)
+                    run()
+            else:
                 with open(os.path.join(TEMP_FILE_DIR, TEMP_CONFIG_FILE_NAME), 'w') as f:
                     config.write(f)
                 run()
-        else:
-            with open(os.path.join(TEMP_FILE_DIR, TEMP_CONFIG_FILE_NAME), 'w') as f:
-                config.write(f)
-            run()
-    os.system(f'rm -rf {os.path.join(TEMP_FILE_DIR, TEMP_CONFIG_FILE_NAME)}')
+        os.system(f'rm -rf {os.path.join(TEMP_FILE_DIR, TEMP_CONFIG_FILE_NAME)}')
     shutdown()
